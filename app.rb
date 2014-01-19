8c47ecffdaa0415b870ae1b24148ed7bc0426afe
Manoj Dayaram <noj@moovweb.com>
2014-01-18 22:08:23 -0800

    using sinatra

---
+++ app.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'sass'
+
+configure do
+  set :haml, { :format => :html5 }
+end
+
+get '/styles/:file.css' do
+  scss params[:file].to_sym
+end
+
+get '/' do
+  haml :index
+end
