5c276dd20cbca3c2c8b2809bb9be5d7f94042f8b
Manoj Dayaram <noj@moovweb.com>
2014-01-19 13:08:08 -0800

    whyyyyyy are you broken in herokugit status!

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+  db = URI.parse(ENV['DATABASE_URL'])
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
