fe0a60cc58a43fccc589467dc3dcbd698559f8d2
Manoj Dayaram <noj@moovweb.com>
2014-01-19 13:19:55 -0800

    omg, please work now

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,12 @@
+require 'active_record'
+
 configure :development do
   set :database, 'sqlite:///db/dev.sqlite3'
   set :show_exceptions, true
 end
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'])
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
