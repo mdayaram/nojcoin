58e785bc687373767dbde085c10d7b7954335fb9
Manoj Dayaram <noj@moovweb.com>
2014-01-19 16:04:51 -0800

    rudimentary stealing!

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@
 require_relative './config/environment'
 require_relative './models/trade'
 
-
 get '/styles/:file.css' do
   scss params[:file].to_sym
 end
@@ -14,4 +13,30 @@ get '/' do
   @value = trade.offer
   @notes = trade.notes
   haml :index
+end
+
+get '/stolen/:id' do
+  @trade = Trade.find(params[:id])
+  haml :stolen
+end
+
+get '/steal' do
+  trade = Trade.order("created_at DESC").first
+  @owner = trade.to
+  haml :steal
+end
+
+post '/steal' do
+  last = Trade.order("created_at DESC").first
+  @trade = Trade.new
+  @trade.to = params[:stealer]
+  @trade.from = last.to
+  @trade.offer = last.offer
+  @trade.notes = "burn!"
+
+  if @trade.save
+    redirect "/stolen/#{@trade.id}"
+  else
+    haml :steal
+  end
 end
